8193037c6a6f1f9e2f84f03fa9e0bf1ef18ba732
kwkw <wei497896143@yeah.net>
2024-06-04 14:56:54 +0800

    feat: add Records

--- src/interaction.rs
+++ src/interaction.rs
@@ -6,10 +6,12 @@ use mockall::automock;
 
 #[automock]
 pub trait InteractionTrait {
+    fn input_common(&self, prompt: &str) -> String;
     fn input_id(&self) -> String;
+    fn input_job_id(&self) -> String;
     fn confirm_delete(&self) -> bool;
 
-    fn input_common(&self, prompt: &str, default: &str, text: &str) -> String;
+    fn input_common_default(&self, prompt: &str, default: &str, text: &str) -> String;
     fn input_name(&self, text: &str) -> String;
     fn input_start_at(&self, text: &str) -> String;
     fn input_interval(&self, text: &str) -> String;
@@ -18,6 +20,9 @@ pub trait InteractionTrait {
     fn input_args(&self, text: &str) -> String;
     fn input_timeout(&self, text: &str) -> String;
     fn input_queues(&self, text: &str) -> String;
+
+    fn input_page(&self, text: &str) -> String;
+    fn input_page_size(&self, text: &str) -> String;
 
     fn select_common(&self, prompt: &str, selections: Vec<String>, default: usize) -> String;
     fn select_type(&self) -> String;
@@ -28,11 +33,21 @@ pub struct Interaction;
 
 impl InteractionTrait for Interaction {
     #[cfg_attr(coverage_nightly, coverage(off))]
-    fn input_id(&self) -> String {
+    fn input_common(&self, prompt: &str) -> String {
         Input::with_theme(&ColorfulTheme::default())
-            .with_prompt("ID")
+            .with_prompt(prompt)
             .interact_text()
             .unwrap()
+    }
+
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    fn input_id(&self) -> String {
+        self.input_common("ID").trim().to_string()
+    }
+
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    fn input_job_id(&self) -> String {
+        self.input_common("JobId").trim().to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
@@ -47,7 +62,7 @@ impl InteractionTrait for Interaction {
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
-    fn input_common(&self, prompt: &str, default: &str, text: &str) -> String {
+    fn input_common_default(&self, prompt: &str, default: &str, text: &str) -> String {
         let mut initial_text = default.to_string();
         if !text.is_empty() {
             initial_text = text.to_string();
@@ -63,43 +78,73 @@ impl InteractionTrait for Interaction {
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_name(&self, text: &str) -> String {
-        self.input_common("Name", "myJob", text)
+        self.input_common_default("Name", "myJob", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_start_at(&self, text: &str) -> String {
         let local_datetime = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        self.input_common("StartAt", &local_datetime, text)
+        self.input_common_default("StartAt", &local_datetime, text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_interval(&self, text: &str) -> String {
-        self.input_common("Interval", "60s", text)
+        self.input_common_default("Interval", "60s", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_cron_expr(&self, text: &str) -> String {
-        self.input_common("CronExpr", "*/1 * * * *", text)
+        self.input_common_default("CronExpr", "*/1 * * * *", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_timezone(&self, text: &str) -> String {
-        self.input_common("Timezone", "UTC", text)
+        self.input_common_default("Timezone", "UTC", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_args(&self, text: &str) -> String {
-        self.input_common("Args", "{}", text)
+        self.input_common_default("Args", "{}", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_timeout(&self, text: &str) -> String {
-        self.input_common("Timeout", "1h", text)
+        self.input_common_default("Timeout", "1h", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn input_queues(&self, text: &str) -> String {
-        self.input_common("Queues", "[\"default\"]", text)
+        self.input_common_default("Queues", "[\"default\"]", text)
+            .trim()
+            .to_string()
+    }
+
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    fn input_page(&self, text: &str) -> String {
+        self.input_common_default("Page", "1", text)
+            .trim()
+            .to_string()
+    }
+
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    fn input_page_size(&self, text: &str) -> String {
+        self.input_common_default("PageSize", "10", text)
+            .trim()
+            .to_string()
     }
 
     #[cfg_attr(coverage_nightly, coverage(off))]
@@ -107,7 +152,7 @@ impl InteractionTrait for Interaction {
         let selection = Select::with_theme(&ColorfulTheme::default())
             .with_prompt(prompt)
             .default(default)
-            .max_length(4)
+            .max_length(8)
             .items(&selections[..])
             .interact()
             .unwrap();
